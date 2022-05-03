6ad3b148a4f758978f20b889eb15884f7c57f7dd
Jason Peixoto <jason.peixoto@carieinc.com>
2022-05-03 14:58:57 -0400

    V1.00.11

---
+++ FenomPlus.SDK/FenomPlus.SDK.Core/Features/FeatureStatus.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FenomPlus.SDK.Core.Features
+{
+    public enum FeatureStatus
+    {
+        Unkown,
+        Success,
+        Disconnected,
+        Timeout
+    }
+}
---
+++ FenomPlus.SDK/FenomPlus.SDK.Core/Models/BasePacket.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace FenomPlus.SDK.Core.Models
+{
+    public class BasePacket
+    {
+        public byte[] Data { get; set; }
+        public bool IsValid { get { return (Data != null) && (Data.Length > 0); } }
+
+        public byte Extract8(byte[] data, int index)
+        {
+            return (data[index]);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        public int Extract16(byte[] data, int index)
+        {
+            return (data[index + 0] << 8) |
+                    (data[index + 1]);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        public static int Extract32(byte[] data, int index)
+        {
+            return (data[index + 0] << 24) |
+                    (data[index + 1] << 16) |
+                    (data[index + 2] << 8) |
+                    (data[index + 3]);
+        }
+    }
+}
