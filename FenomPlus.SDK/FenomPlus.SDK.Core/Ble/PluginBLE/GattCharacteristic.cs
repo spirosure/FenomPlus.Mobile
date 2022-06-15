7336666c9698347e235ade49dbafe9cd79179cde
smacleodcaire <Scott.Macleod@caireinc.com>
2022-06-15 10:35:34 -0700

    can not set characteristic in ble in c#

--- FenomPlus.SDK/FenomPlus.SDK.Core/Ble/PluginBLE/GattCharacteristic.cs
+++ FenomPlus.SDK/FenomPlus.SDK.Core/Ble/PluginBLE/GattCharacteristic.cs
@@ -132,7 +132,7 @@ namespace FenomPlus.SDK.Core.Ble.PluginBLE
                     throw new Exception("Characteristic cannot be written");
                 }
 
-                Characteristic.WriteType = CharacteristicWriteType.WithResponse;
+            //    Characteristic.WriteType = CharacteristicWriteType.WithResponse;
 
                 return await Characteristic.WriteAsync(value);
             }
